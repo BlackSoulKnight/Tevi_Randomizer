93324f00738adb26d0b89320ac4b20c3da463233
BlackSoulKnight <44965005+BlackSoulKnight@users.noreply.github.com>
2025-10-01 16:25:16 +0200

    visibility

--- Randomizer_Plugin/ArchipelagoInterface.cs
+++ Randomizer_Plugin/ArchipelagoInterface.cs
@@ -28,7 +28,7 @@ namespace TeviRandomizer
         public const ItemList.Type remoteItem = ItemList.Type.I10;
         public const ItemList.Type remoteItemProgressive = ItemList.Type.I11;
 
-        string AP_WORLD_VERSION = "0.6.3";
+        public string AP_WORLD_VERSION = "0.6.3";
 
         private class LocationData
         {
