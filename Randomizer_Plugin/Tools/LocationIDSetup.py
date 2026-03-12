7282f16337c1e08252819f03b7102178bde79ba7
BlackSoulKnight <44965005+BlackSoulKnight@users.noreply.github.com>
2026-03-12 18:00:15 +0100

    location IDs

---
+++ Randomizer_Plugin/Tools/LocationIDSetup.py
@@ -0,0 +1,65 @@
+import json
+import os
+import random
+
+
+## Reading main Data files
+
+    
+
+Path = os.path.dirname(os.path.realpath(__file__))
+
+
+def teleporterHub():
+    s = open(Path+"/Area.json",'r')
+    s = json.load(s)
+    a = 0
+    s["TeleporterHub"] = [{"Name":f"TeleportHub","Connections":[]}]
+    for k in s["Teleporter"]:
+        data = {"Exit":k["Name"],"Method":f"Teleporter {a}"}
+        s["TeleporterHub"][0]["Connections"].append(data)
+        a+=1
+    d = open(Path+"Test.json",'w+')
+    d.write(json.dumps(s))
+    d.close
+
+
+def addLocationId():
+    maxID = 0
+    s = open(Path+"/../resource/JsonFiles/Location.json",'r')
+    locations = json.load(s)
+    s.close()
+    s = open(Path+"/../resource/JsonFiles/MoneyLocations.json",'r')
+    moneylocation = json.load(s)
+    s.close() 
+    s = open(Path+"/../resource/JsonFiles/UpgradeResourceLocation.json",'r')
+    resourcelocation = json.load(s)
+    s.close()
+    combined = locations+resourcelocation+moneylocation
+    noIds = [] 
+    for v in combined:
+        if "EVENT_" in v["Itemname"]:
+            v["LocationID"] = 0
+            continue
+        if not"LocationID" in v:
+            noIds.append(v)
+        elif v["LocationID"] > maxID:
+            maxID = v["LocationID"]
+    for v in noIds:
+        maxID +=1
+        v["LocationID"] = maxID
+    s = open(Path+"/../resource/JsonFiles/Location.json",'w+')
+    s.write(json.dumps(locations))
+    s.close()
+    s = open(Path+"/../resource/JsonFiles/MoneyLocations.json",'w+')
+    s.write(json.dumps(moneylocation))
+    s.close()
+    s = open(Path+"/../resource/JsonFiles/UpgradeResourceLocation.json",'w+')
+    s.write(json.dumps(resourcelocation))
+    s.close()
+    
+
+
+        
+addLocationId()
+
